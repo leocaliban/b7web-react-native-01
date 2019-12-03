68ef149b5929b31e75aa9d9ab46c0bc34853e79e
Leocaliban <leocaliban@gmail.com>
2019-12-03 00:26:51 -0300

    Criação de um componente de Texto

---
+++ rnbasics/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const TextoComponent = () => {
+  return <Text>COMPONENT TEXTO WORKS!!!</Text>;
+};
+
+export default TextoComponent;
