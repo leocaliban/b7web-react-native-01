f7fef1babc122099176549ee0fc4e29e22d1b744
Leocaliban <leocaliban@gmail.com>
2019-12-03 01:14:29 -0300

    Estilização de componentes

--- rnbasics/App.js
+++ rnbasics/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView
+} from 'react-native';
 
 // Criação de componente usando constante
 const TextoComponent1 = () => {
@@ -26,4 +31,43 @@ class TextoComponent3 extends Component {
   }
 }
 
-export default TextoComponent;
+const ComponenteComEstilos = () => {
+  return (
+    <View style={styles.page}>
+      <Text style={styles.text}>Estilização de componente com StyleSheet (class)</Text>
+      <Text style={{ color: '#fff896', fontSize: 30, textAlign: 'center' }}>Estilo inline</Text>
+      <Text style={[styles.text, styles.erro]}>Erro!!!</Text>
+    </View>
+  );
+};
+
+// SafeAreaView para enquadrar o item na tela dos dispositivos IOS
+const ComponenteSafeAreaView = () => {
+  return (
+    <SafeAreaView style={styles.page}>
+      <Text style={styles.text}>Estilização de componente com StyleSheet</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  page: {
+    width: '100%',
+    height: 300,
+    backgroundColor: '#478888',
+    borderBottomEndRadius: 50,
+    borderBottomStartRadius: 50
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    textAlign: 'center',
+    marginTop: 50
+  },
+  erro: {
+    color: '#ff8c80'
+  }
+});
+
+export default ComponenteComEstilos;
