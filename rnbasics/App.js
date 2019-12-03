2773d20d0a480e373bba5fd518d5ff6beb4ee378
Leocaliban <leocaliban@gmail.com>
2019-12-03 01:55:49 -0300

    Fix: StyledComponent

--- rnbasics/App.js
+++ rnbasics/App.js
@@ -73,7 +73,7 @@ const styles = StyleSheet.create({
 });
 
 // Criação do componente Content
-const Content = styled.Text`
+const Content = styled.SafeAreaView`
   flex:1
   background-color:black;
 `;
