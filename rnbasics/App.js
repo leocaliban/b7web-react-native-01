cb2facb563821b0fe1a3bf74f349cb76a9f083e7
Leocaliban <leocaliban@gmail.com>
2019-12-03 00:35:55 -0300

    Formas de criação de componentes

--- rnbasics/App.js
+++ rnbasics/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { Component } from 'react';
+import { Text } from 'react-native';
 
-const TextoComponent = () => {
-  return <Text>COMPONENT TEXTO WORKS!!!</Text>;
+// Criação de componente usando constante
+const TextoComponent1 = () => {
+  return (
+    <Text>COMPONENT TEXTO WORKS!!!</Text>
+  );
 };
+
+// Simplificada de 1 linha
+const TextoComponent = () => <Text>COMPONENT TEXTO WORKS!!!</Text>;
+
+function TextoComponent2() {
+  return (
+    <Text>COMPONENT TEXTO WORKS!!!</Text>
+  );
+}
+
+// Criação de componente usando classes (não recomendado)
+class TextoComponent3 extends Component {
+  render() {
+    return (
+      <Text> COMPONENT TEXTO WORKS!!!</Text >
+    );
+  }
+}
 
 export default TextoComponent;
