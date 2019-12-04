a4c8798e8c0e64baacc9ab718ce79bfb80bb71a6
Leocaliban <leocaliban@gmail.com>
2019-12-04 15:55:53 -0300

    Formulário de Login com NativeBase

--- nativeBase/App.js
+++ nativeBase/App.js
@@ -1,114 +1,52 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
+  Button,
   Text,
-  StatusBar,
-} from 'react-native';
-
-import {
+  Container,
   Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  Left,
+  Body,
+  Right,
+  Title,
+  Subtitle,
+  Content,
+  Form,
+  Item,
+  Input
+} from 'native-base';
+import styled from 'styled-components/native';
 
-const App: () => React$Node = () => {
+const Page = styled.SafeAreaView`
+  flex:1
+`;
+
+export default () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <Page>
+      <Container>
+        <Header>
+          <Left />
+          <Body>
+            <Title>Cadastro</Title>
+            <Subtitle>Login</Subtitle>
+          </Body>
+          <Right />
+        </Header>
+
+        <Content style={{ paddingHorizontal: 20, marginTop: 40 }}>
+          <Form>
+            <Item>
+              <Input placeholder="Usuário" />
+            </Item>
+            <Item last>
+              <Input placeholder="Senha" />
+            </Item>
+          </Form>
+          <Button block rounded style={{ marginTop: 20 }}>
+            <Text>SALVAR</Text>
+          </Button>
+        </Content>
+      </Container>
+    </Page>
   );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+}
