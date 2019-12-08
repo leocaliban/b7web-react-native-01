255d115807e263f6b627b99c39a689f8f79c8e24
Leocaliban <leocaliban@gmail.com>
2019-12-08 02:06:25 -0300

    Cria o Navigator e uma tela inicial

---
+++ stackNavigation/src/screens/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Page = styled.SafeAreaView`
+    flex:1;
+    justify-content:center;
+    align-items: center;
+`;
+
+const Titulo = styled.Text`
+    font-size:20px;
+`;
+
+
+export default () => {
+    return (
+        <Page>
+            <Titulo>Tela Inicial!</Titulo>
+        </Page>
+    );
+}
